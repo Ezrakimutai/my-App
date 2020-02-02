966e77a59cca6980e46422130dd0f894e20b2ac4
doublejosh <joshuaklind@gmail.com>
2020-02-02 02:28:42 -0800

    tidy depth and TS syntax

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -13,43 +13,41 @@ interface IntroProps {
     typed?: string[]
 }
 
-const Intro = (props: IntroProps) => {
-    return (
-        <div id="home" className="intro route bg-image background">
-            <div id="stars" />
-            <div id="stars2" />
-            <div id="stars3" />
-            <div className="intro-content display-table">
-                <div className="table-cell">
-                    <Container>
-                        <h1 className="intro-title mb-4">{props.headline}</h1>
-                        <p className="intro-subtitle">
-                            <span className="text-slider-items"></span>
-                            <strong className="text-slider">
-                                <Typed
-                                    strings={props.typed}
-                                    typeSpeed={80}
-                                    backDelay={1100}
-                                    backSpeed={30}
-                                    loop
-                                />
-                            </strong>
-                        </p>
-                        <p className="pt-3">
-                            <a
-                                className="btn btn-primary btn-lg js-scroll px-4"
-                                href="#about"
-                                role="button"
-                            >
-                                <i className="ion-arrow-down-c"></i>
-                            </a>
-                        </p>
-                    </Container>
-                </div>
+export const Intro: React.FC<IntroProps> = props => (
+    <div id="home" className="intro route bg-image background">
+        <div id="stars" />
+        <div id="stars2" />
+        <div id="stars3" />
+        <div className="intro-content display-table">
+            <div className="table-cell">
+                <Container>
+                    <h1 className="intro-title mb-4">{props.headline}</h1>
+                    <p className="intro-subtitle">
+                        <span className="text-slider-items"></span>
+                        <strong className="text-slider">
+                            <Typed
+                                strings={props.typed}
+                                typeSpeed={80}
+                                backDelay={1100}
+                                backSpeed={30}
+                                loop
+                            />
+                        </strong>
+                    </p>
+                    <p className="pt-3">
+                        <a
+                            className="btn btn-primary btn-lg js-scroll px-4"
+                            href="#about"
+                            role="button"
+                        >
+                            <i className="ion-arrow-down-c"></i>
+                        </a>
+                    </p>
+                </Container>
             </div>
         </div>
-    )
-}
+    </div>
+)
 
 const mapStateToProps = (state: RootState) => {
     return state.portfolio.intro

4222cb06ff2cc96ac4070a78a5275baa51b28a96
doublejosh <joshuaklind@gmail.com>
2020-02-03 17:38:57 -0800

    TS reducer

--- src/store/reducers/sampleReducer.ts
+++ src/store/reducers/sampleReducer.ts
@@ -10,30 +10,40 @@ const initState = {
     },
     intro: {
         headline: "Hi. Let's talk.",
-        typed: ["I'm a front-end developer", 'who can edit JSON', 'and deploy it.'],
+        typed: [
+            "I'm a front-end developer",
+            'who can edit JSON',
+            'and deploy it.',
+        ],
     },
     resume: {
         headline: 'Describe yourself in one quick sentence',
-        summary: ['Expand on that information in more depth in a few short paragraphs', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'],
+        summary: [
+            'Expand on that information in more depth in a few short paragraphs',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+        ],
         jobsHeadline: 'Employment',
         jobs: [
             {
                 title: 'VP of Something',
                 company: 'Fancy LLC',
                 timeframe: '2015-2020',
-                summary: "List all the cool stuff you did here. Don't be shy, but be honest and specific.",
+                summary:
+                    "List all the cool stuff you did here. Don't be shy, but be honest and specific.",
             },
             {
                 title: 'Director of Stuff',
                 company: 'Neato Inc.',
                 timeframe: '2010-2015',
-                summary: 'Highlight some accomplishments in these descriptions. Sometimes people even read them.',
+                summary:
+                    'Highlight some accomplishments in these descriptions. Sometimes people even read them.',
             },
             {
                 title: 'Developer',
                 company: 'WorkJob Co.',
                 timeframe: '2005-2010',
-                summary: 'Action works and quantified results help explain what you really did in a job.',
+                summary:
+                    'Action works and quantified results help explain what you really did in a job.',
             },
         ],
         toolsHeadline: 'Skills',
@@ -62,21 +72,24 @@ const initState = {
                 {
                     id: 'section-1-project-1',
                     title: 'My Cool Project',
-                    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+                    summary:
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
                     image: '',
                     slides: [],
                 },
                 {
                     id: 'section-1-project-2',
                     title: 'Another One',
-                    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+                    summary:
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
                     image: '',
                     slides: [],
                 },
                 {
                     id: 'section-1-project-3',
                     title: 'Something Neat',
-                    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+                    summary:
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
                     image: '',
                     slides: [],
                 },
@@ -91,21 +104,24 @@ const initState = {
                 {
                     id: 'section-2-project-1',
                     title: 'Project 1',
-                    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+                    summary:
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
                     image: '',
                     slides: [],
                 },
                 {
                     id: 'section-2-project-2',
                     title: 'Project 2',
-                    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+                    summary:
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
                     image: '',
                     slides: [],
                 },
                 {
                     id: 'section-2-project-3',
                     title: 'Project 3',
-                    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+                    summary:
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
                     image: '',
                     slides: [],
                 },
@@ -132,6 +148,6 @@ const initState = {
     },
 }
 
-export default (state = initState, action) => {
+export default (state = initState) => {
     return state
 }

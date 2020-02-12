72a5c33fa9f0f24af55c649eff5e0af26574e25d
doublejosh <joshuaklind@gmail.com>
2020-02-12 13:18:44 -0800

    use state for nav

--- src/components/topNav/index.tsx
+++ src/components/topNav/index.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import { compose } from 'redux'
 import { RootState } from '../../store/myTypes'
 import { Navbar } from 'react-bootstrap'
+import { NavbarProps } from 'react-bootstrap'
 import NavMenu from './navMenu'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
@@ -11,23 +12,20 @@ import $ from 'jquery'
 import './style.scss'
 
 export const TopNav: React.FC<Portfolio> = props => {
+    const [variant, setVariant] = useState<NavbarProps['variant']>('dark')
+
     useEffect(() => {
         const navbar = document.querySelector('.navbar') as HTMLElement
         const body: any = $('body')
 
         if (!navbar) return
 
-        const toggle = (elm: HTMLElement, added: string, removed: string) => {
-            elm.classList.add(added)
-            elm.classList.remove(removed)
-        }
-
         // Change nav style for load and top.
         window.addEventListener('scroll', () => {
             if (window.pageYOffset > navbar.offsetHeight) {
-                toggle(navbar, 'navbar-light', 'navbar-dark')
+                setVariant('light')
             } else {
-                toggle(navbar, 'navbar-dark', 'navbar-light')
+                setVariant('dark')
             }
         })
 
@@ -43,7 +41,8 @@ export const TopNav: React.FC<Portfolio> = props => {
             collapseOnSelect
             expand="lg"
             fixed="top"
-            className="navbar-dark"
+            role="navigation"
+            variant={variant}
         >
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             {!!props.meta.showRepoLink && (

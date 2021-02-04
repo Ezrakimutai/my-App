ae0c7c7a4ccfa223e28f5ca9449d6316cbe1e930
Josh Lind <joshua.lind@maven.io>
2021-02-03 21:42:47 -0800

    allow html tools lists from json

--- src/components/resume/tool.tsx
+++ src/components/resume/tool.tsx
@@ -5,7 +5,7 @@ export const ToolBox: React.FC<Tool> = props => (
     <p>
         <span className="font-weight-bold">{props.name}</span>
         <br />
-        {props.list}
+        <div dangerouslySetInnerHTML={{ __html: props.list }}></div>
     </p>
 )
 
